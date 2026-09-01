602c93bfb83ce21a6e1af343b9de4fc8a3be7742
Nathan Schmidt <nathanschmidt1908@gmail.com>
2026-09-01 15:49:19 +0200

    added 5

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -1,1 +1,10 @@
 # Names of all students corresponding with their learning team number.
+
+
+def solution_station_5(name):
+    name_dict = {
+        "Soelie": 3,
+        "Yasmin": 1,
+        "Arnav": 3,
+    }
+    print(name_dict.get(name))
