c8f2cf395c7997a4d83982eb7716f253cfdf7898
alexvoelm <alex.vanoel@gmail.com>
2026-09-01 15:27:19 +0200

    Updated station1

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -1,6 +1,11 @@
 def solution_station_1(n):
-    if n <= 1:
+    if n < 0:
+        return -1
+    if n == 0:
+        return 0
+    if n == 1:
         return 1
+
     a, b = 0, 1
     for i in range(2, n + 1):
         a, b = b, a + b
