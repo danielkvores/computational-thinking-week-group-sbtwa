ed66babe438c0c9a0391f4fe37d7f8839b210d32
AinasJacyna <ainasjacyna092321@gmail.com>
2026-09-01 16:19:04 +0200

    Added

--- challenge_day2/station5.py
+++ challenge_day2/station5.py
@@ -6,5 +6,9 @@ def solution_station_5(name):
         "Soelie": 3,
         "Yasmin": 1,
         "Arnav": 3,
+        "Iris" : 2,
+        "Lora" : 2,
+        "Minseo" : 2
     }
+
     return name_dict.get(name)
