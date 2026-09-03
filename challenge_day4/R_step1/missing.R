54cfa0c58f4246ed2b4033c6010fbb7596804ba0
danielkvores <kvoresdaniel@gmail.com>
2026-09-03 13:47:01 +0200

    finished working R file

--- challenge_day4/R_step1/missing.R
+++ challenge_day4/R_step1/missing.R
@@ -17,14 +17,14 @@ for(col_name in names(people_df)) {
   if(is.numeric(people_df[[col_name]])) {
     # Calculate mean, excluding NAs
     col_mean <- mean(people_df[[col_name]], na.rm = TRUE)
-    
+
     # Replace NAs with the calculated mean
     people_df[[col_name]][is.na(people_df[[col_name]])] <- col_mean
   }
 }
 
 # Replace the 'people' list in the original data with the modified data frame
-json_data$people <- person_df
+json_data$people <- people_df
 
 # Convert the updated data back to JSON format
 json_text <- toJSON(json_data, pretty = TRUE)
